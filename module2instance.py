f32fb184ca6d93b92ce5f510b2dc3f632be46547
milosdinic <133354110+milosdinic@users.noreply.github.com>
2023-12-15 12:38:27 +0100

    Add files via upload

--- module2instance.py
+++ module2instance.py
@@ -22,6 +22,7 @@ with open('module.txt','r') as oldfile , open('module_name.txt', 'w') as newfile
            break
         last_element = line.rsplit(delimiter, 1)[-1]
         substituted_phrase = last_element.replace("module", "" )
+        substituted_phrase = last_element.replace("endmodule", "" )
         substituted_phrase = substituted_phrase.replace("(", "" )
         substituted_phrase = substituted_phrase.replace("#(", "" )
         newfile.write(substituted_phrase)
@@ -67,6 +68,7 @@ with open('port_list_filter.txt','r') as oldfile , open('wire_list_new.txt', 'w'
         substituted_phrase = substituted_phrase.replace(",", ";" )
         substituted_phrase = substituted_phrase.replace("_i", "_s" )
         substituted_phrase = substituted_phrase.replace("_o", "_s" )
+        substituted_phrase = substituted_phrase.replace("reg", "wire" )
         newfile.write(substituted_phrase.lstrip())
 
 with open('wire_list_new.txt', 'r') as f:
@@ -155,11 +157,104 @@ with open('instance.txt', 'w') as f:
     f.writelines(data)
 
 
+############## PARAMS START ######################################
+with open(filename, 'r') as f, open ('params.txt', 'w') as nf:
+    for line in f:
+        if '#(' in line:
+            for line in f:
+                nf.write(line)
+                if ')' in line:
+                  break
+
+with open('params.txt','r') as oldfile , open('params1.txt', 'w') as newfile:
+    while True:
+        line = oldfile.readline()
+        if not line:
+           break
+        last_element = line
+        substituted_phrase = last_element.replace("#(", "" )
+        substituted_phrase = substituted_phrase.replace(")", "" )
+        substituted_phrase = substituted_phrase.replace("parameter", "" )
+        newfile.write(substituted_phrase)
+
+with open('params1.txt','r') as f , open('params2.txt', 'w') as newf:
+    while True:
+        line = f.readline()
+        if not line:
+           break
+        x = line.partition('=')
+        line = x[0]
+        new_line = line.replace(" ", "" )
+        newf.write(new_line)
+        newf.write('\n')
+
+with open('params2.txt','r+') as f , open('params3.txt', 'w') as newf:
+    for line in f:
+        if line.rstrip() or not line.isspace():
+            newf.write(line)
+par=1
+if os.stat("params2.txt").st_size == 0:
+    par=0
+
+
+data5 = ""
+with open ('params3.txt') as f:
+    data5 = f.read()
+
+maxparamLen = len(max(open('params3.txt').readlines(), key=len))
+minparamLen = len(min(open('params3.txt').readlines(), key=len))
+data5 = ""
+
+with open('params3.txt') as f:
+    port_lines = f.read().splitlines()
+with open('params4.txt', 'w') as newf:
+    for line in port_lines:
+        Len = len(line)
+        print('     .' + line + " "*(maxparamLen+2-Len) + '( ' + line + " "*(3*maxparamLen-minparamLen-Len) + '),', file=newf)
+
+
+with open('params4.txt', 'r') as f:
+    for count, line in enumerate(f):
+        pass
+
+with open('params4.txt', 'r') as f: 
+    data = f.readlines() 
+data[count] = data[count].replace("),", ") )" )
+  
+with open('params5.txt', 'w') as f: 
+    f.writelines(data)
+
+paramdata1 = "( \n"
+paramdata2 = ""
+
+with open ('params5.txt') as f:
+    paramdata2 = f.read()
+
+paramdata1 += paramdata2
+ 
+with open ('parameters.txt', 'w') as f:
+    f.write(paramdata1)
+    
+paramdata1 = ""
+paramdata2 = ""
+
+############## PARAMS END   ######################################
+
+
+############## COMBINE ALL START #################################
+data5 = ""
+data4 = ""
 data3 = ""
 data2 = ""
 data1 = ""
-with open('module_name_n.txt') as file:
+with open('parameters.txt') as file:
+    data5 = file.read()
+
+with open('module_name.txt') as file:
     data3 = file.read()
+
+with open('module_name_instance.txt') as file:
+    data4 = file.read()
 
 with open('wire_list.txt') as file:
     data1 = file.read()
@@ -170,10 +265,9 @@ with open('instance.txt') as f2:
 data1 += "\n"
 data1 += "\n"
 data1 += data3
-data1 += "\n"
-data1 += "// #(  ) //CHECK FOR PARAMETERS!"
-data1 += "\n"
-data1 += "\n"
+if par:
+    data1 += data5
+data1 += data4
 data1 += "("
 data1 += "\n"
 data1 += data2
@@ -181,9 +275,14 @@ data1 += data2
 with open ('module_instance.txt', 'w') as f:
     f.write(data1)
 
+data5 = ""
+data4 = ""
 data3 = ""
 data2 = ""
 data1 = ""
+
+
+
 ########## REMOVE ALL THE MESS ###########
 if os.path.exists('port_list_filter.txt'):
     os.remove('port_list_filter.txt')
@@ -197,6 +296,8 @@ if os.path.exists('port_list_clean3.txt'):
     os.remove('port_list_clean3.txt')
 if os.path.exists('port_list_clean4.txt'):
     os.remove('port_list_clean4.txt')
+if os.path.exists('wire_list1.txt'):
+    os.remove('wire_list1.txt')
 if os.path.exists('wire_list.txt'):
     os.remove('wire_list.txt')
 if os.path.exists('wire_list_new.txt'):
@@ -213,6 +314,15 @@ if os.path.exists('module_name.txt'):
     os.remove('module_name.txt')
 if os.path.exists('module_name_n.txt'):
     os.remove('module_name_n.txt')
-
-
-
+if os.path.exists('params.txt'):
+    os.remove('params.txt')
+if os.path.exists('params1.txt'):
+    os.remove('params1.txt')
+if os.path.exists('params2.txt'):
+    os.remove('params2.txt')
+if os.path.exists('params3.txt'):
+    os.remove('params3.txt')
+if os.path.exists('params4.txt'):
+    os.remove('params4.txt')
+if os.path.exists('params5.txt'):
+    os.remove('params5.txt')
